4abab8712961c219ff0886ea1ca379e6e1c9f1c8
Ilham Mulia <dev.ilhammulia@gmail.com>
2023-05-11 22:35:34 +0700

    Edit Pins

--- resources/js/Pages/Pins.vue
+++ resources/js/Pages/Pins.vue
@@ -6,13 +6,33 @@
             </h2>
         </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <h1 class="text-xl">Create your pins</h1>
-                <div class="">
-                    <!-- Insert Code here -->
+        <div class="flex">
+
+            <div class="mx-auto py-12 max-w-7xl" :class="{ 'blur-sm': isVisible === true }">
+                <div class="flex justify-between mx-auto sm:px-6 lg:px-8">
+                    <SearchBar class="text-gray-600 h-4 w-4 fill-current" />
+                    <ShowModeSwitch @mode-toggle="changeShowMode" />
+                </div>
+                <div class="flex">
+                    <!-- Pin Card List -->
+                    <div class="mx-auto sm:px-6 lg:px-8 py-5">
+                        <div v-if="showMode === 'grid'" class="grid grid-cols-3 grid-flow-row gap-10">
+                            <CreatePinCard @modal-toggle="modalToggle" />
+                            <div v-for="pin in pins" :key="pin.id">
+                                <PinCard :pin="pin" @update-pin="updatePin" />
+                            </div>
+                        </div>
+
+                        <div v-if="showMode === 'list'" class="grid grid-flow-row gap-7">
+                            <div v-for="pin in pins" :key="pin.id">
+                                <PinCardList :pin="pin" @update-pin="updatePin" />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <!-- Sidebar Modal for Create Pin -->
+            <PinModal :isVisible="isVisible" @close-modal="modalToggle" :pinToUpdate="pinToUpdate" ref="modal" />
         </div>
     </AppLayout>
 </template>
@@ -20,14 +40,42 @@
 <script>
 import AppLayout from '@/Layouts/AppLayout.vue';
 import Welcome from '@/Components/Welcome.vue';
+import PinCard from '@/Components/PinCard.vue';
+import PinCardList from '@/Components/PinCardList.vue';
+import CreatePinCard from '@/Components/CreatePinCard.vue';
+import PinModal from '@/Components/PinModal.vue';
+import ShowModeSwitch from '@/Components/ShowModeSwitch.vue';
+import SearchBar from '@/Components/SearchBar.vue';
 
 export default {
-    setup() {
-        
+    data() {
+        return { isVisible: false, pinToUpdate: null, showMode: 'grid' }
     },
     components: {
         AppLayout,
-        Welcome
+        Welcome,
+        PinCard,
+        PinCardList,
+        CreatePinCard,
+        PinModal,
+        PinModal,
+        ShowModeSwitch,
+        SearchBar
+    },
+    props: {
+        pins: Array
+    },
+    methods: {
+        modalToggle(isVisible) {
+            this.isVisible = !isVisible
+        },
+        updatePin(value) {
+            this.pinToUpdate = value
+            this.isVisible = true
+        },
+        changeShowMode(value) {
+            this.showMode = value
+        }
     }
 }
 </script>
