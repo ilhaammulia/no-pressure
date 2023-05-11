7ce48bce7a27d1f25d86ae4534ae5cec05586e1e
Ilham Mulia <dev.ilhammulia@gmail.com>
2023-05-11 22:36:49 +0700

    Add PinDropdown component

---
+++ resources/js/Components/PinDropdown.vue
@@ -0,0 +1,57 @@
+<script>
+  import Dropdown from '@/Components/Dropdown.vue'
+  import DropdownLink from '@/Components/DropdownLink.vue';
+  import DeleteIcons from '@/Icons/DeleteIcons.vue';
+  import ElipsisIcons from '@/Icons/ElipsisIcons.vue';
+  import { useForm } from '@inertiajs/inertia-vue3';
+
+  export default {
+    setup(props) {
+      const form = useForm({
+        id: props.pin.id
+      });
+
+      const remove = () => {
+        form.delete(route('pin.destroy', { id: form.id }));
+      }
+
+      return { form,  remove}
+    },
+    methods: {
+      updatePin() {
+        this.$emit('update-pin', this.pin)
+      },
+    },
+    components: { Dropdown, DropdownLink, DeleteIcons, ElipsisIcons },
+    props: ['pin']
+  }
+</script>
+
+
+<template>
+  <Dropdown align="right">
+    <template #trigger>
+      <button class="text-xs">
+        <ElipsisIcons class="w-4 h-4 fill-gray-400" />
+      </button>
+    </template>
+    <template #content>
+      <div class="block">
+        <DropdownLink as="button">
+          <button @click="updatePin" class="w-full text-start text-xs text-gray-400">Edit</button>
+        </DropdownLink>
+      </div>
+      <div class="border-t border-gray-100" />
+      <div class="block">
+        <form @submit.prevent="remove">
+          <DropdownLink as="button">
+            <div class="flex items-center gap-1">
+              <DeleteIcons class="w-3 h-3 fill-red-400" />
+              <button class="w-full text-start text-xs text-red-400">Delete</button>
+            </div>
+          </DropdownLink>
+        </form>
+      </div>
+    </template>
+  </Dropdown>
+</template>
