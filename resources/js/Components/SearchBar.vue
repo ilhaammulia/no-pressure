80503088202291dd5434f85e4be2a9762bb91272
Ilham Mulia <dev.ilhammulia@gmail.com>
2023-05-11 22:37:35 +0700

    Add search bar component

---
+++ resources/js/Components/SearchBar.vue
@@ -0,0 +1,17 @@
+<script>
+import TextInput from './TextInput.vue';
+
+export default {
+  name: 'SearchBar',
+  components: { TextInput },
+  props: {
+    classList: String
+  }
+}
+</script>
+
+<template>
+  <div class="text-gray-600">
+    <TextInput class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none" placeholder="Search" />
+  </div>
+</template>
