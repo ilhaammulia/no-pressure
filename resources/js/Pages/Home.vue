a47e5a7492da965a4447d3dcf64012537e8f86e4
Ilham Mulia <dev.ilhammulia@gmail.com>
2023-05-11 22:35:02 +0700

    Edit Home

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,22 +1,25 @@
 <template>
-    <section class="min-h-screen bg-stone-300 relative">
+    <section class="min-h-screen relative">
         <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
             <Link v-if="$page.props.user" :href="route('pins')" class="text-sm text-gray-700 dark:text-gray-500 underline">Pins</Link>
 
             <template v-else>
-                <Link :href="route('login')" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</Link>
-
-                <Link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</Link>
+                <div class="mr-10">
+                    <Link :href="route('login')" class="bg-flatpurple hover:bg-purple-600 text-white font-semibold py-3 px-8 rounded inline-flex items-center mt-5">Log In</Link>
+                </div>
             </template>
         </div>
 
-        <div class="flex w-full container mx-auto flex-col py-10 gap-10">
-            <h1 class="text-3xl">Pins</h1>
+        <div class="flex w-full container mx-auto flex-col py-20 gap-10">
+            <h1 class="text-3xl dark:text-white font-extrabold">Pins</h1>
 
-            <div v-for="pin in pins" :key="pin.id" class="bg-white px-5 py-2 rounded-md">
-                <h1 class="pb-5">by: <span class="font-semibold text-indigo-600">{{ pin.user_name }}</span></h1>
-                <h1 class="text-xl font-bold">{{ pin.title }}</h1>
-                <p>{{ pin.description }}</p>
+            <div class="flex justify-between mt-5">
+                <div class="">
+                    <h1 class="text-black font-bold text-6xl">Stay organized with our digital pinboard</h1>
+                    <p class="mt-3 text-lg">By using our digital pinboard, users can easily capture their ideas, prioritize their tasks, and declutter their physical workspace. With our user-friendly interface, staying organized has never been easier.</p>
+                    <Link v-if="canRegister" :href="route('register')" class="bg-flatpurple hover:bg-purple-600 text-white font-semibold py-3 px-8 rounded-full inline-flex items-center mt-5">Get Started</Link>
+                </div>
+                <img src="../../img/peoples-2d.png" class="w-2/3 -mr-40">
             </div>
         </div>
     </section>
